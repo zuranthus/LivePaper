009bbf1c59f160431b5c6c22807ef400203b8352
zuranthus <7449346+zuranthus@users.noreply.github.com>
2021-10-18 13:50:41 +0700

    Replace deprecated ffmpeg API

--- video.c
+++ video.c
@@ -3,65 +3,35 @@
 
 #include <SDL.h>
 
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 
 static AVCodecContext *pCodecCtx = NULL;
-static AVCodecContext *pCodecCtxOrig = NULL;
 static     AVFormatContext *pFormatCtx = NULL;
-static     AVFrame *pFrame = NULL;
 static struct SwsContext *sws_ctx = NULL;
+static AVFrame *pFrame = NULL;
 static int videoStream=-1;
 
 void VideoLoad(char *path, int w, int h) {
-    // Open video file
     if(avformat_open_input(&pFormatCtx, path, NULL, NULL)!=0)
         FAIL_WITH("Couldn't open file");
 
-    // Retrieve stream information
     if(avformat_find_stream_info(pFormatCtx, NULL)<0)
         FAIL_WITH("Couldn't find stream information");
 
-    int i;
-
-
-    // Find the first video stream
-    for(i=0; i<pFormatCtx->nb_streams; i++)
-    if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-        videoStream=i;
-        break;
-    }
-    if(videoStream==-1)
-        FAIL(); // Didn't find a video stream
-
-    // Get a pointer to the codec context for the video stream
-    pCodecCtxOrig=pFormatCtx->streams[videoStream]->codec;
-    assert(pCodecCtxOrig);
-
     AVCodec *pCodec = NULL;
+    videoStream = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, &pCodec, 0);
+    if (videoStream < 0) FAIL(); // Didn't find a video stream
 
-    // Find the decoder for the video stream
-    pCodec=avcodec_find_decoder(pCodecCtxOrig->codec_id);
-    if(pCodec==NULL) {
-        FAIL_WITH("Unsupported codec!\n");
-    }
-    // Copy context
     pCodecCtx = avcodec_alloc_context3(pCodec);
-    if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
-        FAIL_WITH("Couldn't copy codec context");
-    }
-    // Open codec
+    assert(pCodecCtx);
+    avcodec_parameters_to_context(pCodecCtx, pFormatCtx->streams[videoStream]->codecpar);
     if(avcodec_open2(pCodecCtx, pCodec, NULL)<0)
         FAIL();
 
-    // Allocate video frame
-    pFrame=av_frame_alloc();
-    assert(pFrame);
-    
-    // initialize SWS context for software scaling
+    pFrame = av_frame_alloc();
+
     sws_ctx = sws_getContext(
         pCodecCtx->width,
         pCodecCtx->height,
@@ -76,41 +46,34 @@ void VideoLoad(char *path, int w, int h) {
 }
 
 void VideoNextFrame(SDL_Texture *tex) {
-    int i=0;
-    AVPacket packet;
-    int frameFinished = 0;
-    while(av_read_frame(pFormatCtx, &packet)>=0) {
-        // Is this a packet from the video stream?
-        if(packet.stream_index==videoStream) {
-            // Decode video frame
-            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
-           
-            // Did we get a video frame?
-            if(frameFinished) {
+    for(AVPacket packet; av_read_frame(pFormatCtx, &packet) >= 0;) {
+        if (packet.stream_index == videoStream) {
+            int ret = avcodec_send_packet(pCodecCtx, &packet);
+            if (ret < 0) FAIL();
+
+            for (;;) {
+                ret = avcodec_receive_frame(pCodecCtx, pFrame);
+                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) break;
+                else if (ret < 0) FAIL();
+
                 void*pix;
                 int pitch;
                 SDL_LockTexture(tex, NULL, &pix, &pitch);
                 sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
                     pFrame->linesize, 0, pCodecCtx->height,
                     (uint8_t* []){pix, NULL, NULL}, (int [] ){pitch});
-               SDL_UnlockTexture(tex);
+                SDL_UnlockTexture(tex);
             }
+            break;
         }
 
-        // Free the packet that was allocated by av_read_frame
-        av_free_packet(&packet);
-        if (frameFinished) break;
+        av_packet_unref(&packet);
     }
 }
 
 void VideoUnload() {
-    // Free the YUV frame
-    av_free(pFrame);
-
-    // Close the codecs
+    sws_freeContext(sws_ctx);
+    av_frame_free(&pFrame);
     avcodec_close(pCodecCtx);
-    avcodec_close(pCodecCtxOrig);
-
-    // Close the video file
     avformat_close_input(&pFormatCtx);
 }
