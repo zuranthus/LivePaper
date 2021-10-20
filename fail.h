5435171b508fa963580bb534cec7aa731d4f8637
zuranthus <7449346+zuranthus@users.noreply.github.com>
2021-10-20 21:22:53 +0700

    Include file name in FAIL message

--- fail.h
+++ fail.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void MessageAndQuit(int line, const char *message, ...);
+void MessageAndQuit(int line, const char *file, const char *message, ...);
 
-#define FAIL_WITH(M, ...) MessageAndQuit(__LINE__, M, ##__VA_ARGS__)
-#define FAIL() MessageAndQuit(__LINE__, NULL)
+#define FAIL_WITH(M, ...) MessageAndQuit(__LINE__, __FILE__, M, ##__VA_ARGS__)
+#define FAIL() MessageAndQuit(__LINE__, __FILE__, NULL)
