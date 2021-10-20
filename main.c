9730e08a2c0fae5369748a05dcdfdd69807edb08
zuranthus <7449346+zuranthus@users.noreply.github.com>
2021-10-20 20:11:38 +0700

    Remove flipbook support

--- main.c
+++ main.c
@@ -1,68 +1,13 @@
 #include <assert.h>
 #include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 #include <SDL.h>
-#define CUTE_PNG_IMPLEMENTATION
-#include <cute_png.h>
-#define CUTE_FILES_IMPLEMENTATION
-#include <cute_files.h>
 
 #include "fail.h"
 #include "platform.h"
-
-typedef struct {
-    SDL_Texture **textures;
-    int count;
-    int w, h;
-} Textures;
-
-Textures LoadTextures(const char *dir_path, SDL_Renderer *renderer) {
-    Textures textures;
-    memset(&textures, 0, sizeof(textures));
-    int max_textures = 8;
-    textures.textures = malloc(max_textures * sizeof(*textures.textures));
-
-    cf_dir_t dir;
-    if (!cf_dir_open(&dir, dir_path))
-        FAIL_WITH("can't open directory '%s'", dir_path);
-    while (dir.has_next) {
-        cf_file_t file;
-        cf_read_file(&dir, &file);
-        if (file.is_reg && cf_match_ext(&file, ".png")) {
-            cp_image_t image = cp_load_png(file.path);
-            if (!image.pix) FAIL_WITH("can't load file %s", file.path);
-            if (textures.w == 0 || textures.h == 0) {
-                textures.w = image.w;
-                textures.h = image.h;
-            }
-            if (image.w != textures.w || image.h != textures.h) {
-                FAIL_WITH("previous image(s) are %ix%i pix, but '%s' is %ix%i pix; all images must have identical dimensions", 
-                    textures.w, textures.h, file.path, image.w, image.h);
-            }
-            SDL_Texture *tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, image.w, image.h);
-            if (!tex) FAIL_WITH("can't create SDL texture %i x %i pix", image.w, image.h);
-            if (SDL_UpdateTexture(tex, NULL, image.pix, image.w*4) != 0) 
-                FAIL();
-
-            if (textures.count == max_textures) {
-                max_textures *= 2;
-                textures.textures = realloc(textures.textures, max_textures * sizeof(*textures.textures));
-            }
-            textures.textures[textures.count++] = tex;
-            free(image.pix);
-        }
-        cf_dir_next(&dir);
-    }
-    cf_dir_close(&dir);
-
-    return textures;
-}
-
-void ClearTextures(Textures *textures) {
-    free(textures->textures);
-    memset(textures, 0, sizeof(Textures));
-}
 
 Context CreateContext() {
     Context context;
@@ -85,38 +30,21 @@ void ClearContext(Context *context) {
 }
 
 
-void VideoLoad(char *path, int w, int h);
-bool VideoNextFrame(SDL_Texture *tex, double delta_sec);
-void VideoUnload();
+struct Video* VideoLoad(char *path, const Context *context);
+bool VideoNextFrame( double delta_sec, struct Video *video, const Context *context);
+void VideoClear(struct Video *video);
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) FAIL();
-    // if (argc != 3) {
-    //     puts("USAGE: animated-wallpaper path-to-dir delay-in-ms");
-    //     puts("EXAMPLE: ./animated-wallpaper /home/wallpaper/ 100");
-    //     return -1;
-    // }
+    if (argc != 2) {
+        puts("USAGE: animated-wallpaper path-to-file");
+        puts("EXAMPLES: ./animated-wallpaper ~/wallpaper/fuji.gif");
+        puts("          ./animated-wallpaper ~/wallpaper/video.mp4");
+        return -1;
+    }
 
     Context context = CreateContext();
-    // Textures textures = LoadTextures(argv[1], context.renderer);
-    // if (textures.count == 0) FAIL_WITH("No images loaded from '%s'", argv[1]);
-    // const int delay = atoi(argv[2]);
-
-    int win_w, win_h;
-    SDL_GetWindowSize(context.window, &win_w, &win_h);
-    VideoLoad(argv[1], win_w, win_h);
-    SDL_Texture *tex = SDL_CreateTexture(context.renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, win_w, win_h);
-        if (!tex) FAIL_WITH("can't create SDL texture %i x %i pix", win_w, win_h);
-    // const float aspect_ratio = fmaxf((float)win_w/textures.w, (float)win_h/textures.h);
-    // const int target_w = (int)(textures.w * aspect_ratio);
-    // const int target_h = (int)(textures.h * aspect_ratio);
-    // const SDL_Rect dest_rect = {
-    //     .x = (win_w - target_w)/2,
-    //     .y = (win_h - target_h)/2,
-    //     .w = target_w,
-    //     .h = target_h
-    // };
-    // int curframe = -1;
+    struct Video* video = VideoLoad(argv[1], &context);
+    
     double ticks_frequency = (double)SDL_GetPerformanceFrequency();
     uint64_t ticks_now = SDL_GetPerformanceCounter();
     uint64_t ticks_last = 0;
@@ -124,18 +52,8 @@ int main(int argc, char *argv[]) {
         ticks_last = ticks_now;
         ticks_now = SDL_GetPerformanceCounter();
         double delta_sec = (ticks_now - ticks_last)/ticks_frequency;
-        // int frame = (SDL_GetTicks() / delay) % textures.count;
-        // if (curframe != frame) {
-        //     curframe = frame;
-        //     assert(curframe < textures.count);
-        if (VideoNextFrame(tex, delta_sec)) {
-                SDL_SetRenderDrawColor(context.renderer, 0, 0, 0, 255);
-                SDL_RenderClear(context.renderer);
-            //     SDL_RenderCopy(context.renderer, textures.textures[curframe], NULL, &dest_rect);
-            SDL_RenderCopy(context.renderer, tex, NULL, NULL);
-                SDL_RenderPresent(context.renderer);
-            // }
-        }
+
+        VideoNextFrame(delta_sec, video, &context);
         
         fflush(stdout);
         PlatformUpdate(&context);
@@ -150,9 +68,7 @@ int main(int argc, char *argv[]) {
         SDL_Delay(1);
     }
 
-    SDL_DestroyTexture(tex);
-   // ClearTextures(&textures);
+    VideoClear(video);
     ClearContext(&context);
-    VideoUnload();
     return 0;
 }
--- video.c
+++ video.c
@@ -8,55 +8,85 @@
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 
-static AVCodecContext *decoder_ctx = NULL;
-static AVFormatContext *input_ctx = NULL;
-static struct SwsContext *sws_ctx = NULL;
-static AVFrame *frame = NULL;
-static int video_stream=-1;
-static double current_time = 0.0;
-static double total_time = 1.0;
-static double current_frame_end_time = -1.0;
-static double video_time_base;
+#include "platform.h"
 
-void VideoLoad(char *path, int w, int h) {
-    if (avformat_open_input(&input_ctx, path, NULL, NULL)!=0)
+struct Video {
+    AVCodecContext *decoder_ctx;
+    AVFormatContext *input_ctx;
+    struct SwsContext *sws_ctx;
+    int video_stream;
+    double total_time;
+    double video_time_base;
+
+    double current_time;
+    double current_frame_end_time;
+
+    AVFrame *frame;
+    SDL_Texture *tex;
+    SDL_Rect tex_dest;
+};
+
+struct Video* VideoLoad(char *path, const Context *context) {
+    struct Video *v = av_mallocz(sizeof(struct Video));
+
+    if (avformat_open_input(&v->input_ctx, path, NULL, NULL)!=0)
         FAIL_WITH("Couldn't open file");
-    if (avformat_find_stream_info(input_ctx, NULL)<0)
+    if (avformat_find_stream_info(v->input_ctx, NULL)<0)
         FAIL_WITH("Couldn't find stream information");
 
     AVCodec *codec = NULL;
-    video_stream = av_find_best_stream(input_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
-    if (video_stream < 0) FAIL();
+    v->video_stream = av_find_best_stream(v->input_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
+    if (v->video_stream < 0) FAIL();
 
-    decoder_ctx = avcodec_alloc_context3(codec);
-    assert(decoder_ctx);
-    AVStream *stream = input_ctx->streams[video_stream];
-    avcodec_parameters_to_context(decoder_ctx, stream->codecpar);
-    if(avcodec_open2(decoder_ctx, codec, NULL)<0)
+    v->decoder_ctx = avcodec_alloc_context3(codec);
+    assert(v->decoder_ctx);
+    AVStream *stream = v->input_ctx->streams[v->video_stream];
+    avcodec_parameters_to_context(v->decoder_ctx, stream->codecpar);
+    if(avcodec_open2(v->decoder_ctx, codec, NULL)<0)
         FAIL();
-    video_time_base = av_q2d(stream->time_base);
-    total_time = stream->duration*video_time_base;
+    v->video_time_base = av_q2d(stream->time_base);
+    v->total_time = stream->duration*v->video_time_base;
 
-    frame = av_frame_alloc();
+    v->frame = av_frame_alloc();
 
-    sws_ctx = sws_getContext(
-        decoder_ctx->width,
-        decoder_ctx->height,
-        decoder_ctx->pix_fmt,
-        w, h,
-        AV_PIX_FMT_RGB24,
+    int w = v->decoder_ctx->width;
+    int h = v->decoder_ctx->height;
+    v->sws_ctx = sws_getContext(
+        w, h, v->decoder_ctx->pix_fmt,
+        w, h, AV_PIX_FMT_RGB24,
         SWS_BILINEAR,
         NULL,
         NULL,
         NULL
     );
+
+    v->tex = SDL_CreateTexture(context->renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, w, h);
+    if (!v->tex) FAIL_WITH("can't create SDL texture %i x %i pix", w, h);
+
+    int win_w, win_h;
+    SDL_GetWindowSize(context->window, &win_w, &win_h);
+    const float aspect_ratio = fmaxf((float)win_w/w, (float)win_h/h);
+    const int target_w = (int)(w * aspect_ratio);
+    const int target_h = (int)(h * aspect_ratio);
+    const SDL_Rect dest_rect = {
+        .x = (win_w - target_w)/2,
+        .y = (win_h - target_h)/2,
+        .w = target_w,
+        .h = target_h
+    };
+    v->tex_dest = dest_rect;
+
+    return v;
 }
 
-bool VideoNextFrame(SDL_Texture *tex, double delta_sec) {
-    current_time += delta_sec;
+bool VideoNextFrame(double delta_sec, struct Video *v, const Context *context) {
+    assert(v);
+    assert(v->input_ctx);
 
-    printf("\nUPDATE %.3f/%.2f\n", current_time, current_frame_end_time);
-    if (current_time < current_frame_end_time) {
+    v->current_time += delta_sec;
+
+    printf("\nUPDATE %.3f/%.2f\n", v->current_time, v->current_frame_end_time);
+    if (v->current_time < v->current_frame_end_time) {
         printf("skipping\n");
         return false;
     }
@@ -66,49 +96,49 @@ bool VideoNextFrame(SDL_Texture *tex, double delta_sec) {
 
     for(;;) {
         AVPacket packet;
-        int ret = av_read_frame(input_ctx, &packet);
+        int ret = av_read_frame(v->input_ctx, &packet);
         if (ret == AVERROR_EOF) {
             printf("EOF ");
-            if (current_time < total_time) {       
+            if (v->current_time < v->total_time) {       
                 done = true;
                 printf("waiting for end of video\n");
             } else {
-                current_time = fmod(current_time, total_time);
-                current_frame_end_time = -1.0;
-                avcodec_flush_buffers(decoder_ctx);
-                av_seek_frame(input_ctx, video_stream, 0, AVSEEK_FLAG_BACKWARD);
-                printf(" restarting video from time %.2f\n", current_time);
+                v->current_time = fmod(v->current_time, v->total_time);
+                v->current_frame_end_time = -1.0;
+                avcodec_flush_buffers(v->decoder_ctx);
+                av_seek_frame(v->input_ctx, v->video_stream, 0, AVSEEK_FLAG_BACKWARD);
+                printf(" restarting video from time %.2f\n", v->current_time);
                 continue;
             }
         } else if (ret < 0) {
             FAIL();
         }
 
-        if (!done && packet.stream_index == video_stream) {
-            if (avcodec_send_packet(decoder_ctx, &packet) < 0) FAIL();
+        if (!done && packet.stream_index == v->video_stream) {
+            if (avcodec_send_packet(v->decoder_ctx, &packet) < 0) FAIL();
             for (;;) {
-                int ret = avcodec_receive_frame(decoder_ctx, frame);
+                int ret = avcodec_receive_frame(v->decoder_ctx, v->frame);
                 if (ret == AVERROR(EAGAIN)) break; // read new frame with av_read_frame and try decoding again
                 else if (ret < 0) FAIL();
 
-                int64_t pts = frame->best_effort_timestamp;
+                int64_t pts = v->frame->best_effort_timestamp;
                 if (pts == AV_NOPTS_VALUE) FAIL_WITH("No pts value. Unsupported video codec or corrupted file.");
-                double frame_end_time = (pts + frame->pkt_duration)*video_time_base;
-                assert(frame_end_time >= current_frame_end_time);
+                double frame_end_time = (pts + v->frame->pkt_duration)*v->video_time_base;
+                assert(frame_end_time >= v->current_frame_end_time);
 
-                found_frame = (current_time <= frame_end_time);
+                found_frame = (v->current_time <= frame_end_time);
 
                 if (found_frame) {
                     done = true;
-                    current_frame_end_time = frame_end_time;
+                    v->current_frame_end_time = frame_end_time;
 
                     void*pix;
                     int pitch;
-                    SDL_LockTexture(tex, NULL, &pix, &pitch);
-                    sws_scale(sws_ctx, (uint8_t const * const *)frame->data,
-                        frame->linesize, 0, decoder_ctx->height,
+                    SDL_LockTexture(v->tex, NULL, &pix, &pitch);
+                    sws_scale(v->sws_ctx, (uint8_t const * const *)v->frame->data,
+                        v->frame->linesize, 0, v->decoder_ctx->height,
                         (uint8_t* []){pix, NULL, NULL}, (int [] ){pitch});
-                    SDL_UnlockTexture(tex);
+                    SDL_UnlockTexture(v->tex);
                     printf("new frame %.2f\n", frame_end_time);
                 } else {
                     printf("skipping frame %.2f\n", frame_end_time);
@@ -124,12 +154,21 @@ bool VideoNextFrame(SDL_Texture *tex, double delta_sec) {
 
     if (!found_frame) printf("\nno frame found\n");
 
+
+    if (found_frame) {
+        SDL_SetRenderDrawColor(context->renderer, 0, 0, 0, 255);
+        SDL_RenderClear(context->renderer);
+        SDL_RenderCopy(context->renderer, v->tex, NULL, &v->tex_dest);
+        SDL_RenderPresent(context->renderer);
+    }
     return found_frame;
 }
 
-void VideoUnload() {
-    sws_freeContext(sws_ctx);
-    av_frame_free(&frame);
-    avcodec_close(decoder_ctx);
-    avformat_close_input(&input_ctx);
+void VideoClear(struct Video *v) {
+    sws_freeContext(v->sws_ctx);
+    av_frame_free(&v->frame);
+    avcodec_close(v->decoder_ctx);
+    avformat_close_input(&v->input_ctx);
+    memset(v, 0, sizeof(*v));
+    av_free(v);
 }
