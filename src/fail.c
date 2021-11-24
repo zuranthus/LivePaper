c14f65176f93fc053011dfe31ff54cd1a03fbf48
zuranthus <7449346+zuranthus@users.noreply.github.com>
2021-11-25 00:38:06 +0700

    Display errors as message boxes on Windows

--- src/fail.c
+++ src/fail.c
@@ -4,16 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 void MessageAndQuit(int line, const char *file, const char *message, ...) {
     va_list args;
     va_start(args, message);
 
-    printf("Error %s:%i", file, line);
+    char buf[4096] = {0};
+    int pos = 0;
+    pos += sprintf(buf + pos, "Error %s:%i", file, line);
     if (message) {
-        printf(": ");
-        vprintf(message, args);
+        pos += sprintf(buf + pos, ": ");
+        pos += vsprintf(buf + pos, message, args);
     }
-    puts("\nExiting...");
+    va_end(args);
+#ifdef _WIN32
+    MessageBoxA(NULL, buf, "LivePaper Error", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+    puts(buf);
+    puts("Exiting...");
     fflush(stdout);
+#endif
+    
     exit(1);
 }
